b6c4647d7a525de057e9a4f32ff9d6db3b760e4f
junlv <39781664@qq.com>
2019-03-29 18:48:18 +0800

    update

--- scripts/compile_less.js
+++ scripts/compile_less.js
@@ -1,7 +1,6 @@
 const { rm, cp, mkdir, exec, echo } = require('shelljs');
 const chalk = require('chalk');
 
-
 console.log(chalk.blue('编译 LESS ./src/less/main.less'));
 mkdir('-p', './dist/static/css');
 exec('lessc ./src/less/main.less ./dist/static/css/main.css');
